ea5b8e6fdc814821eb1c9d0c6ca5f7fd1969d2ea
ben.vulich <ben.vulich@URB218-02.seasad.wustl.edu>
2017-10-09 12:44:01 -0500

    Studio 6

---
+++ debounce/debounce.ino
@@ -0,0 +1,48 @@
+const int buttonPin = 2;
+
+int pressCount = 0;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 5;
+
+
+int lastButtonState = LOW;
+int buttonState = LOW;
+unsigned long nextCountTime = 0;
+
+void buttonPressed() {
+  //Serial.println("1");
+  int reading = digitalRead(buttonPin);
+  if(reading != lastButtonState){
+    //Serial.println("2");
+    lastDebounceTime = millis();
+  }
+  
+  lastButtonState = reading;
+}
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  // Interrupts can happen on "edges" of signals.  
+  // Three edge types are supported: CHANGE, RISING, and FALLING 
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, CHANGE);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if((millis()-lastDebounceTime)>debounceDelay){
+    int reading = digitalRead(buttonPin);
+    if(buttonState == LOW&&reading==HIGH){
+      ++pressCount;
+      //Serial.println("4");
+    }
+    
+    buttonState = reading;
+
+  }
+  if(millis()>nextCountTime){
+    Serial.println(pressCount);
+    nextCountTime += 1000;
+  }
+  
+}
--- src/studio6/SerialComm.java
+++ src/studio6/SerialComm.java
@@ -25,12 +25,80 @@ public class SerialComm {
 		
 		debug = false; // Default is to NOT be in debug mode
 	}
+	
+	public void writeByte(byte b){
+		try {
+			port.writeByte(b);
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (debug){
+			/*int first = b/16;
+			//System.out.println(b);
+			//System.out.println(first);
+			char firstChar;
+			if(first<10){
+				firstChar = (char) (first+0x30);
+			} else {
+				firstChar = (char) (first+0x60);
+			}
+			int second = b%16;
+			//System.out.println(second);
+			char secondChar;
+			if(second<10){
+				secondChar = (char) (second+0x30);
+			} else {
+				secondChar = (char) (second+0x60-9);
+			}
+			System.out.println("<0x"+firstChar+secondChar+">");*/
+			System.out.println("<0x"+String.format("%02x", b)+">");
+			
+		}
+	}
 		
 	// TODO: Add writeByte() method from Studio 5
 	
+	public boolean available() {
+		try {
+			return port.getInputBufferBytesCount()>0;
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
 	// TODO: Add available() method
 	
+	byte readByte() {
+		try {
+			byte b =  port.readBytes()[0];
+			if(debug) {
+				System.out.println("<0x"+String.format("%02x", b)+">");
+			}
+			return b;
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+	}
 	// TODO: Add readByte() method	
 	
 	// TODO: Add a main() method
+	public static void main(String[] args) {
+		SerialComm x;
+		try {
+			x = new SerialComm("COM7");
+		
+			while(true) {
+				if(x.available()) {
+					System.out.print((char)x.readByte());
+				}
+			}
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
